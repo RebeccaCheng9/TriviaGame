84baa27bd8cdace52b1005db56148c73716ad19f
RebeccaCheng9 <rc780288@gmail.com>
2026-04-03 17:52:44 -0400

    Change font to Raleway, Add fun facts for each question

--- trivia_game/app/page.tsx
+++ trivia_game/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useState } from 'react';
+import "tailwindcss";
+
 interface Question {
     question: string;
     options: string[];
     answer: string;
+    funfact: string;
 }
 
 const questions: Question[] = [
@@ -12,16 +15,55 @@ const questions: Question[] = [
         question: "When was RPI established?",
         options: ["1800", "1820", "1814", "1824"],
         answer: "1824",
+        funfact: "Did you know that RPI is the oldest technological research university in the US!",
+    },
+    {
+        question: "True or False: Majoring in mechanical engineering was always offered.",
+        options: ["True", "False"],
+        answer: "False",
+        funfact: "Degrees in electrical and mechanical engineering were first offered in the 1910s.",
+    },
+    {
+        question: "The founders of RPI are...",
+        options: ["Stephen Van Rensselaer and Palmer Ricketts", "Stephen Van Rensselaer and Russell Sage", "Stephen Van Rensselaer and Benjamin Franklin Greene", "Stephen Van Rensselaer and Amos Eaton"],
+        answer: "Stephen Van Rensselaer and Amos Eaton",
+        funfact: "Stephen Van Rensselaer was an influential politician who served in the House of Representatives from 1822 - 1829.",
+    },
+    {
+        question: "The Polytechnic, the school newspaper, has had continous publication since what year?",
+        options: ["1850", "1824", "1885", "1900"],
+        answer: "1885",
+        funfact: "You are actually using the Polytechnic website right now!!",
+    },
+    {
+        question: "The 1950 graduating class is unique due to the fact that...?",
+        options: ["Over 80% were veterans", "Over 60% were female", "Over 70% were atheletes", "Over 50% were younger than 20"],
+        answer: "Over 80% were veterans",
+        funfact: "Additionally, the average age was 32, and over 40% were married.",
+    },
+    {
+        question: "George W. G. Ferris (RPI Class of 1881) is known for creating the first...?",
+        options: ["Swing Tower", "Rollercoaster", "Merry-go-Round", "Ferris Wheel"],
+        answer: "Ferris Wheel",
+        funfact: "Another notible RPI alumni include Steven Sasson (class of 1972), the inventor of the first digital camera.",
+    },
+    {
+        question: "True or False: Puckman was not the first mascot.",
+        options: ["True", "False"],
+        answer: "True",
+        funfact: "The mascot of RPI has gone through multiple changes, including ones like \"The Bachelors,\" and \"Red Hawks\"."
     },
     {
         question: "What was RPI's original name?",
         options: ["Rensselaer School", "Rensselaer Institute", "Rensselaer University", "Rensselaer Polytechnic Institute"],
         answer: "Rensselaer School",
+        funfact: "All the options above were once the name of RPI except for Rensselaer University!",
     },
     {
         question: "The freshman dorms were completed in the:",
         options: ["1940s", "1950s", "1960s", "1970s"],
         answer: "1950s",
+        funfact: "The freshman dorms have been around for over 75 years!",
     },
 ];
 
@@ -36,6 +78,15 @@ const QuizApp: React.FC = () => {
         }
         handleNextQuestion();
     };
+    /* 
+    const revealAnswer = (option: string) => {
+        questions[currentQuestion].answer.style.background = #00d627;
+        if (option === questions[currentQuestion].answer) {
+            setScore(score + 1);
+        }
+
+    }
+    */ 
 
     const handleNextQuestion = () => {
         const nextQuestion = currentQuestion + 1;
@@ -51,10 +102,10 @@ const QuizApp: React.FC = () => {
         container: {
             display: "flex",
             flexDirection: "column" as "column",
-            alignItems: "center",
+            alignItems: "center", 
             justifyContent: "center",
             minHeight: "100vh",
-            background: "linear-gradient(to bottom right, #D6001C, #ffffff)",
+            background: "linear-gradient(to bottom, #D6001C 0%, #e8e8e8 95%, #ffffff 100%)",
             padding: "20px",
             color: "white",
         },
@@ -64,19 +115,14 @@ const QuizApp: React.FC = () => {
             padding: "20px",
             borderRadius: "20px",
             boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.3)",
-            width: "400px",
+            width: "40%",
             textAlign: "center" as "center",
         },
         questionTitle: {
-            fontSize: "1.5rem",
+            fontFamily: "Raleway",
+            fontSize: "2rem",
             fontWeight: "bold",
             color: "#D6001C",
-            marginBottom: "15px",
-        },
-        timer: {
-            fontSize: "1.2rem",
-            fontWeight: "bold",
-            color: "red",
             marginBottom: "15px",
         },
         optionsContainer: {
@@ -86,7 +132,8 @@ const QuizApp: React.FC = () => {
         },
         optionButton: {
             padding: "12px 16px",
-            fontSize: "1rem",
+            fontFamily: "Raleway",
+            fontSize: "1.5rem",
             background: "#D6001C",
             color: "white",
             border: "none",
@@ -99,10 +146,12 @@ const QuizApp: React.FC = () => {
         },
         resultContainer: {
             textAlign: "center" as "center",
+            fontFamily: "Raleway",
         },
         restartButton: {
             marginTop: "20px",
             padding: "12px 16px",
+            fontFamily: "Raleway",
             fontSize: "1rem",
             background: "#D6001C",
             color: "white",
